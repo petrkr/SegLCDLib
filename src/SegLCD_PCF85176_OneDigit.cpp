05e452728f7fc29298cebc4efa8831665e99572e
Petr Kracik <petrkr@petrkr.net>
2025-05-03 19:25:22 +0200

    OneDigit: More charset in ABCD EFGH order

--- src/SegLCD_PCF85176_OneDigit.cpp
+++ src/SegLCD_PCF85176_OneDigit.cpp
@@ -7,20 +7,6 @@ SegLCD_PCF85176_OneDigit::SegLCD_PCF85176_OneDigit(TwoWire& i2c, uint8_t address
 // For now it supports only one controller with 5 digits
 #define DIGIT_COUNT 5
 
-// Segments mapping order ABCD EFGH
-const uint8_t segment_num[10] = {
-    0xFC,
-    0x60,
-    0xDA,
-    0xF2,
-    0x66,
-    0xB6,
-    0xBE,
-    0xE0,
-    0xFE,
-    0xF6
-};
-
 void SegLCD_PCF85176_OneDigit::begin() {
     begin(false);
 }
@@ -124,24 +110,73 @@ int SegLCD_PCF85176_OneDigit::_countDigits(long num) {
     return count;
 }
 
+// TODO: Move to to some generic class and impement mapping
+//Mapping ABCD EFGH
 uint8_t SegLCD_PCF85176_OneDigit::_get_char_value(char ch) {
     switch (ch) {
-        case ' ':
-            return 0x00;
-
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            return segment_num[ch - '0'];
-
-        default:
-            return 0x00; // Return 0x00 for unsupported characters
+        case ' ': return 0x00;
+        case '0': return 0b11111100;
+        case '1': return 0b01100000;
+        case '2': return 0b11011010;
+        case '3': return 0b11110010;
+        case '4': return 0b01100110;
+        case '5': return 0b10110110;
+        case '6': return 0b10111110;
+        case '7': return 0b11100000;
+        case '8': return 0b11111110;
+        case '9': return 0b11110110;
+        case 'A':
+        case 'a': return 0b11101110;
+        case 'B':
+        case 'b': return 0b00111110;
+        case 'C': return 0b10011100;
+        case 'c': return 0b00011010;
+        case 'D':
+        case 'd': return 0b01111010;
+        case 'E':
+        case 'e': return 0b10011110;
+        case 'F':
+        case 'f': return 0b10001110;
+        case 'G':
+        case 'g': return 0b11110110;
+        case 'H': return 0b01101110;
+        case 'h': return 0b00101110;
+        case 'I': return 0b01100000;
+        case 'i': return 0b00100000;
+        case 'J': return 0b01111000;
+        case 'j': return 0b01111000;
+        case 'K':
+        case 'k': return 0b10101110;
+        case 'L': return 0b00011100;
+        case 'l': return 0b00011000;
+        case 'M':
+        case 'm': return 0b10101010;
+        case 'N': return 0b11101100;
+        case 'n': return 0b00101010;
+        case 'O': return 0b11111100;
+        case 'o': return 0b00111010;
+        case 'P':
+        case 'p': return 0b11001110;
+        case 'Q':
+        case 'q': return 0b11100110;
+        case 'R':
+        case 'r': return 0b00001010;
+        case 'S':
+        case 's': return 0b10110110;
+        case 'T':
+        case 't': return 0b00011110;
+        case 'U': return 0b01111100;
+        case 'u': return 0b00111000;
+        case 'V':
+        case 'v': return 0b01010100;
+        case 'W':
+        case 'w': return 0b01010110;
+        case 'X': return 0b01101110;
+        case 'x': return 0b00101000;
+        case 'Y':
+        case 'y': return 0b01110110;
+        case 'Z': return 0b11011010;
+        case 'z': return 0b11011000;
+        default: return 0x00;
     }
 }
