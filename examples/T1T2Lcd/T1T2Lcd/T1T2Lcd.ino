c08884f66be536ba342d4cf3675a399db32903f7
Petr Kracik <petrkr@petrkr.net>
2025-05-10 00:13:14 +0200

    T1T2Lcd: Updated example

--- examples/T1T2Lcd/T1T2Lcd/T1T2Lcd.ino
+++ examples/T1T2Lcd/T1T2Lcd/T1T2Lcd.ino
@@ -20,6 +20,22 @@ void setup() {
   delay(500);
 //lcd.write((uint8_t)0xff, 4);
 
+  lcd.setClockSymbol(true);
+
+  delay(1000);
+  lcd.setClockColon(true);
+
+  delay(1000);
+
+  lcd.writeChar(1, '1', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_CLOCK);
+  lcd.writeChar(2, '2', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_CLOCK);
+  lcd.writeChar(3, '2', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_CLOCK);
+  lcd.writeChar(4, '4', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_CLOCK);
+
+  lcd.writeChar(1, '5', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_DAY);
+
+  delay(500);
+
   lcd.setLabels(SegLCD_PCF85176_T1T2Lcd::LABEL_T2_HUM | SegLCD_PCF85176_T1T2Lcd::LABEL_T1_C_UP);
   lcd.writeChar(1, 'h');
   lcd.writeChar(2, 'e');
@@ -34,37 +50,29 @@ void setup() {
   lcd.setT1T2Labels(SegLCD_PCF85176_T1T2Lcd::LABEL_T1);
   delay(500);
 
-  lcd.writeChar(1, 'a', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_CLOCK);
-  lcd.writeChar(2, 'h', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_CLOCK);
-  lcd.writeChar(3, 'o', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_CLOCK);
-  lcd.writeChar(4, 'j', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_CLOCK);
-
-  lcd.writeChar(1, '5', SegLCD_PCF85176_T1T2Lcd::LCDSections::SECTION_DAY);
-
-  delay(500);
   lcd.setT1T2Labels(SegLCD_PCF85176_T1T2Lcd::LABEL_T2);
-
   delay(1000);
+
   lcd.clearT1T2Labels(SegLCD_PCF85176_T1T2Lcd::LABEL_T2);
   
 }
 
 void loop() {
   // Example Signal level
-/*  lcd.setSignalLevel(0);delay(500);
+  lcd.setSignalLevel(0);delay(500);
   lcd.setSignalLevel(1);delay(500);
   lcd.setSignalLevel(2);delay(500);
   lcd.setSignalLevel(3);delay(500);
   lcd.setSignalLevel(4);delay(500);
-  lcd.setSignalLevel(5);delay(500); */
+  lcd.setSignalLevel(5);delay(500);
 
   // Example Battery level
-/*
+
   lcd.setBatteryLevel(0);delay(500);
   lcd.setBatteryLevel(1);delay(500);
   lcd.setBatteryLevel(2);delay(500);
   lcd.setBatteryLevel(3);delay(500);
-  lcd.setBatteryLevel(4);delay(500); */
+  lcd.setBatteryLevel(4);delay(500);
 
   
 }
