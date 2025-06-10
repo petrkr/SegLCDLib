254cb545270fbc419469c4a72ebb441085944caf
Petr Kracik <petrkr@petrkr.net>
2025-06-10 17:44:05 +0200

    TempHum: default zero decimals

--- src/SegLCD_PCF85176_TempHum.h
+++ src/SegLCD_PCF85176_TempHum.h
@@ -25,7 +25,7 @@ class SegLCD_PCF85176_TempHumidity : public SegDriver_PCF85176 {
         void setLabels(uint8_t labels);
         void clearLabels(uint8_t labels);
         void setDecimal(uint8_t digit, bool state, LCDSections section = LCDSections::SECTION_DEFAULT);
-        void writeFloat(float input, uint8_t decimals, LCDSections section = LCDSections::SECTION_DEFAULT);
+        void writeFloat(float input, uint8_t decimals = 0, LCDSections section = LCDSections::SECTION_DEFAULT);
         void writeChar(uint8_t digit, char c, LCDSections section = LCDSections::SECTION_DEFAULT) override;
 
     private:
