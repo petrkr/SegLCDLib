cf1ec2d6b4d0b978df5d90c14984a3a205b7ec00
Petr Kracik <petrkr@petrkr.net>
2025-07-27 14:55:04 +0200

    Removed duplicated code

--- src/SegLCD_HT1621_6SegBat.h
+++ src/SegLCD_HT1621_6SegBat.h
@@ -13,8 +13,6 @@ class SegLCD_HT1621_6SegBat : public SegDriver_HT1621 {
         void setDecimal(uint8_t digit, bool state, LCDSections section = LCDSections::SECTION_DEFAULT);
         void setCursor(uint8_t row, uint8_t col) override;
         size_t write(uint8_t ch) override;
-        void setCursor(uint8_t row, uint8_t col) override;
-        size_t write(uint8_t ch) override;
 
     private:
         uint8_t _buffer_default[6] = {0};
