d9fbc5ef9c94c875925827709072a9c644faebe2
Petr Kracik <petrkr@petrkr.net>
2025-07-29 19:34:53 +0200

    OneDigit: rewrited to LcdApi

--- src/SegLCD_PCF85176_OneDigit.cpp
+++ src/SegLCD_PCF85176_OneDigit.cpp
@@ -3,17 +3,18 @@
 
 SegLCD_PCF85176_OneDigit::SegLCD_PCF85176_OneDigit(TwoWire& i2c, uint8_t address, uint8_t subaddress) :  SegDriver_PCF85176(i2c, address, subaddress) {}
 
-//TODO: Support for chaining multiple displays
-// For now it supports only one controller with 5 digits
-#define DIGIT_COUNT 5
-
 void SegLCD_PCF85176_OneDigit::init() {
     init(false);
 }
 
-void SegLCD_PCF85176_OneDigit::init(bool v1fix) {
+void SegLCD_PCF85176_OneDigit::init(bool reverse) {
+    init(reverse, false);
+}
+
+void SegLCD_PCF85176_OneDigit::init(bool reverse, bool v1fix) {
     SegDriver_PCF85176::init();
     _setMode(MODE_STATUS_ENABLED, MODE_DRIVE_STATIC);
+    _reverse = reverse;
     _v1fix = v1fix;
 }
 
@@ -22,82 +23,58 @@ void SegLCD_PCF85176_OneDigit::clear() {
     SegDriver_PCF85176::clear();
 }
 
-void SegLCD_PCF85176_OneDigit::writeChar(uint8_t digit, char c, LCDSections section) {
-    uint8_t b = _get_char_value(c);
-
-    if (_v1fix) {
-        bool bit_2 = (b >> 2) & 1;
-        bool bit_3 = (b >> 3) & 1;
-
-        if (bit_2 != bit_3) {
-            b ^= (1 << 2) | (1 << 3);
-        }
+void SegLCD_PCF85176_OneDigit::setDecimal(uint8_t row, uint8_t col, bool state) {
+    if (row != 0) {
+        return; // invalid digit
     }
 
-    _buffer[(digit-1)] = b;
-    _writeRam(b, (digit-1)*8);
-}
-
-void SegLCD_PCF85176_OneDigit::setDecimal(uint8_t digit, bool state, LCDSections section) {
-    if (digit < 1 || digit > 5) {
+    if (col < DECIMAL_MIN_COL || col > DECIMAL_MAX_COL) {
         return; // Invalid digit
     }
 
     if (state) {
-        _buffer[(digit-1)] |= 0x01; // Set the decimal point bit
+        _buffer[col] |= DECIMAL_POINT_BIT; // Set the decimal point bit
     } else {
-        _buffer[(digit-1)] &= ~0x01; // Clear the decimal point bit
+        _buffer[col] &= ~DECIMAL_POINT_BIT; // Clear the decimal point bit
     }
 
-    _writeRam(_buffer[(digit-1)], (digit-1)*8);
-}
-
-void SegLCD_PCF85176_OneDigit::writeFloat(float input, uint8_t decimals, LCDSections section) {
-    clear();
-
-    bool isNegative = input < 0.0f;
-    float scale = powf(10, decimals);
-    long scaled = lroundf(fabsf(input) * scale);
-
-    int totalDigits = _countDigits(scaled);
-    int digitPos = 1;
-
-    for (int i = 0; i < totalDigits; ++i) {
-        int digit = scaled % 10;
-        writeChar(digitPos, digit+'0');
-
-        if (i == decimals && decimals > 0) {
-            setDecimal(digitPos, true);
-        }
-
-        scaled /= 10;
-        digitPos++;
-    }
-
-    if (isNegative) {
-        writeChar(digitPos, '-');
-        digitPos++;
+    if (_reverse) {
+        _writeRam(_buffer[col], (DIGITS - (col + 1)) * 8);
+    } else {
+        _writeRam(_buffer[col], col * 8);
     }
 }
 
-// Write string from left to right, support '.' as decimal point (attached to previous digit)
-void SegLCD_PCF85176_OneDigit::writeString(const char* str, LCDSections section) {
-    clear();
-    int len = strlen(str);
-    int digitIndex = 0;
-
-    for (int i = 0; i < len && digitIndex < DIGIT_COUNT; ++i) {
-        if (str[i] == '.') {
-            continue;
-        }
-
-        int digit = DIGIT_COUNT - digitIndex;
-        writeChar(digit, str[i], section);
-
-        if (i + 1 < len && str[i + 1] == '.') {
-            setDecimal(digit, true, section);
-        }
-
-        digitIndex++;
+size_t SegLCD_PCF85176_OneDigit::write(uint8_t ch) {
+    if (_cursorCol < 0 || _cursorCol > DIGITS) {
+        return 0; //Invalid digit
     }
+
+    if (ch == '.') {
+        setDecimal(_cursorRow, _cursorCol - 1, true);
+        return 1;
+    }
+
+    uint8_t segment_data = _get_char_value(ch);
+
+    // Board version v1 have swapped two segments
+    if (_v1fix) {
+        bool bit_2 = (segment_data >> 2) & 1;
+        bool bit_3 = (segment_data >> 3) & 1;
+
+        if (bit_2 != bit_3) {
+            segment_data ^= (1 << 2) | (1 << 3);
+        }
+    }
+
+    _buffer[_cursorCol] = segment_data;
+
+    if (_reverse) {
+        _writeRam(_buffer[_cursorCol], (DIGITS - (_cursorCol + 1)) * 8);
+    } else {
+        _writeRam(_buffer[_cursorCol], _cursorCol * 8);
+    }
+
+    _cursorCol++;
+    return 1;
 }
--- src/SegLCD_PCF85176_OneDigit.h
+++ src/SegLCD_PCF85176_OneDigit.h
@@ -12,14 +12,22 @@ class SegLCD_PCF85176_OneDigit : public SegDriver_PCF85176 {
     public:
         SegLCD_PCF85176_OneDigit(TwoWire& i2c, uint8_t address = DEFAULT_PCF85176_I2C_ADDRESS, uint8_t subaddress = DEFAULT_SUBADDRESS);
         void init() override;
-        void init(bool v1fix);
+        void init(bool reverse);
+        void init(bool reverse, bool v1fix);
         void clear() override;
-        void setDecimal(uint8_t digit, bool state, LCDSections section = LCDSections::SECTION_DEFAULT);
-        void writeChar(uint8_t digit, char c, LCDSections section = LCDSections::SECTION_DEFAULT);
-        void writeFloat(float input, uint8_t decimals = 0, LCDSections section = LCDSections::SECTION_DEFAULT);
-        void writeString(const char* str, LCDSections section = LCDSections::SECTION_DEFAULT);
+        void setDecimal(uint8_t row, uint8_t col, bool state);
+        size_t write(uint8_t ch) override;
     private:
         uint8_t _buffer[MAX_ADDRESS/8];
+        bool _previousDot = false;
+
+        // Support for reverse arranged digits
+        bool _reverse;
         bool _v1fix;
+
+        static constexpr uint8_t DIGITS = 5; // TODO: Do it somehow dynamic as this can be dynamic and this is just maximum
+        static constexpr uint8_t DECIMAL_POINT_BIT = 0x01;
+        static constexpr uint8_t DECIMAL_MIN_COL = 0;
+        static constexpr uint8_t DECIMAL_MAX_COL = DIGITS-1; // All digits supports decimal
 };
 #endif
