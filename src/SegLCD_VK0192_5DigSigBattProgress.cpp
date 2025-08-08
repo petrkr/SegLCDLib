69e066aaa3ae8feb9f302b97ec5ce425377f5ebd
Petr Kracik <petrkr@petrkr.net>
2025-08-08 17:18:14 +0200

    VK0192_5DigSigBattProgress: Implemented setBattery, setSignal and setProgress

--- src/SegLCD_VK0192_5DigSigBattProgress.cpp
+++ src/SegLCD_VK0192_5DigSigBattProgress.cpp
@@ -16,6 +16,103 @@ void SegLCD_VK0192_5DigSigBattProgress::clear() {
     _writeRam(_buffer, sizeof(_buffer), 0);
 }
 
+void SegLCD_VK0192_5DigSigBattProgress::setBatteryLevel(uint8_t level) {
+    if (level > MAX_BATTERY_LEVEL)
+        level = MAX_BATTERY_LEVEL;
+
+    _buffer[ADDR_BATT] &= ~(BATTERY_MASK);
+
+    if (level > 0)
+        _buffer[ADDR_BATT] |= BATTERY_LEVEL_SEG[0];
+    if (level > 1)
+        _buffer[ADDR_BATT] |= BATTERY_LEVEL_SEG[1];
+    if (level > 2)
+        _buffer[ADDR_BATT] |= BATTERY_LEVEL_SEG[2];
+    if (level > 3)
+        _buffer[ADDR_BATT] |= BATTERY_LEVEL_SEG[3];
+
+    _writeRam(_buffer[ADDR_BATT], ADDR_BATT * 2);
+}
+
+void SegLCD_VK0192_5DigSigBattProgress::setSignalLevel(uint8_t level) {
+    if (level > MAX_SIGNAL_LEVEL)
+        level = MAX_SIGNAL_LEVEL;
+
+    _buffer[ADDR_SIGNAL_LOW] &= ~(SIGNAL_MASK_LOW);
+    _buffer[ADDR_SIGNAL_HIGH] &= ~(SIGNAL_MASK_HIGH);
+
+    if (level > 0)
+        _buffer[ADDR_SIGNAL_LOW] |= SIGNAL_LEVEL_SEG[0];
+    if (level > 1)
+        _buffer[ADDR_SIGNAL_LOW] |= SIGNAL_LEVEL_SEG[1];
+    if (level > 2)
+        _buffer[ADDR_SIGNAL_HIGH] |= SIGNAL_LEVEL_SEG[2];
+
+    _writeRam(_buffer[ADDR_SIGNAL_LOW], ADDR_SIGNAL_LOW * 2);
+    _writeRam(_buffer[ADDR_SIGNAL_HIGH], ADDR_SIGNAL_HIGH * 2);
+}
+
+void SegLCD_VK0192_5DigSigBattProgress::setProgress(uint8_t value) {
+    _buffer[ADDR_PROGRESS_P1] &= ~(PROGRESS_MASK);
+    _buffer[ADDR_PROGRESS_P2] &= ~(PROGRESS_MASK);
+    _buffer[ADDR_PROGRESS_P3] &= ~(PROGRESS_MASK);
+    _buffer[ADDR_PROGRESS_P4] &= ~(PROGRESS_MASK);
+
+    if (value > 0) {
+        _buffer[ADDR_PROGRESS_P3] |= PROGRESS_LEVEL_SEG[0];
+    }
+    if (value >= 10) {
+        _buffer[ADDR_PROGRESS_P1] |= PROGRESS_LEVEL_SEG[1];
+    }
+    if (value >= 20) {
+        _buffer[ADDR_PROGRESS_P1] |= PROGRESS_LEVEL_SEG[2];
+    }
+    if (value >= 30) {
+        _buffer[ADDR_PROGRESS_P1] |= PROGRESS_LEVEL_SEG[3];
+    }
+    if (value >= 40) {
+        _buffer[ADDR_PROGRESS_P1] |= PROGRESS_LEVEL_SEG[4];
+    }
+    if (value >= 50) {
+        _buffer[ADDR_PROGRESS_P2] |= PROGRESS_LEVEL_SEG[5];
+    }
+    if (value >= 60) {
+        _buffer[ADDR_PROGRESS_P2] |= PROGRESS_LEVEL_SEG[6];
+    }
+    if (value >= 70) {
+        _buffer[ADDR_PROGRESS_P2] |= PROGRESS_LEVEL_SEG[7];
+    }
+    if (value >= 80) {
+        _buffer[ADDR_PROGRESS_P2] |= PROGRESS_LEVEL_SEG[8];
+    }
+    if (value >= 90) {
+        _buffer[ADDR_PROGRESS_P3] |= PROGRESS_LEVEL_SEG[9];
+    }
+    if (value >= 100) {
+        _buffer[ADDR_PROGRESS_P3] |= PROGRESS_LEVEL_SEG[10];
+    }
+    if (value >= 110) {
+        _buffer[ADDR_PROGRESS_P3] |= PROGRESS_LEVEL_SEG[11];
+    }
+    if (value >= 120) {
+        _buffer[ADDR_PROGRESS_P4] |= PROGRESS_LEVEL_SEG[12];
+    }
+    if (value >= 130) {
+        _buffer[ADDR_PROGRESS_P4] |= PROGRESS_LEVEL_SEG[13];
+    }
+    if (value >= 140) {
+        _buffer[ADDR_PROGRESS_P4] |= PROGRESS_LEVEL_SEG[14];
+    }
+    if (value == 150) {
+        _buffer[ADDR_PROGRESS_P4] |= PROGRESS_LEVEL_SEG[15];
+    }
+
+    _writeRam(_buffer[ADDR_PROGRESS_P1], ADDR_PROGRESS_P1 * 2);
+    _writeRam(_buffer[ADDR_PROGRESS_P2], ADDR_PROGRESS_P2 * 2);
+    _writeRam(_buffer[ADDR_PROGRESS_P3], ADDR_PROGRESS_P3 * 2);
+    _writeRam(_buffer[ADDR_PROGRESS_P4], ADDR_PROGRESS_P4 * 2);
+}
+
 void SegLCD_VK0192_5DigSigBattProgress::setCursor(uint8_t row, uint8_t col) {
     SegDriver_VK0192::setCursor(row, col);
 }
--- src/SegLCD_VK0192_5DigSigBattProgress.h
+++ src/SegLCD_VK0192_5DigSigBattProgress.h
@@ -9,6 +9,9 @@ class SegLCD_VK0192_5DigSigBattProgress : public SegDriver_VK0192 {
         SegLCD_VK0192_5DigSigBattProgress(uint8_t chipselect, uint8_t data, uint8_t write, uint8_t read = -1);
         void init() override;
         void clear() override;
+        void setBatteryLevel(uint8_t value);
+        void setSignalLevel(uint8_t value);
+        void setProgress(uint8_t value);
         void setCursor(uint8_t row, uint8_t col) override;
         void setDecimal(uint8_t row, uint8_t col, bool state);
         size_t write(uint8_t ch) override;
@@ -24,7 +27,33 @@ class SegLCD_VK0192_5DigSigBattProgress : public SegDriver_VK0192 {
         
         static constexpr uint8_t DECIMAL_POINT_BIT = 0x10;
         static constexpr uint8_t NUM_7SEG_DIGITS = 10;  // 3+2+5 7-seg digits
-        
+
+        static constexpr uint8_t MAX_SIGNAL_LEVEL = 3;
+        static constexpr uint8_t ADDR_SIGNAL_LOW = 0x10;
+        static constexpr uint8_t SIGNAL_MASK_LOW = 0xC0;
+        static constexpr uint8_t ADDR_SIGNAL_HIGH = 0x16;
+        static constexpr uint8_t SIGNAL_MASK_HIGH = 0x80;
+        static constexpr uint8_t SIGNAL_LEVEL_SEG[3] = {0x40, 0x80, 0x80};
+
+        static constexpr uint8_t MAX_BATTERY_LEVEL = 4;
+        static constexpr uint8_t ADDR_BATT = 0x11;
+        static constexpr uint8_t BATTERY_MASK = 0xF0;
+        static constexpr uint8_t BATTERY_LEVEL_SEG[4] = {0x10, 0x80, 0x40, 0x20};
+
+        static constexpr uint8_t MAX_PROGRESS = 150;
+        static constexpr uint8_t ADDR_PROGRESS_P1 = 0x10;
+        static constexpr uint8_t ADDR_PROGRESS_P2 = 0x11;
+        static constexpr uint8_t ADDR_PROGRESS_P3 = 0x12;
+        static constexpr uint8_t ADDR_PROGRESS_P4 = 0x13;
+        static constexpr uint8_t PROGRESS_MASK  = 0x0F;
+        static constexpr uint8_t PROGRESS_LEVEL_SEG[16] =
+            { 0x01,
+              0x08, 0x04, 0x02, 0x01,
+              0x01, 0x02, 0x04, 0x08,
+              0x08, 0x04, 0x02,
+              0x08, 0x04, 0x02, 0x01 };
+
+
         uint8_t _mapSegments(uint8_t val);
         int8_t _get7SegmentsAddress(uint8_t row, uint8_t col);
 };
