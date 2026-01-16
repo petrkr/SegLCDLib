258bb8ba8cf46a7838b25523ffde3c1e9b970e2b
Petr Kracik <petrkr@petrkr.net>
2026-01-16 02:58:59 +0100

    64digit: initial example

---
+++ examples/PCF85176/4Seg6SegMaintSegBatUnits/4Seg6SegMaintSegBatUnits.ino
@@ -0,0 +1,68 @@
+/**
+ * @example PCF85176/RawLCD/RawLCD.ino
+ * @brief Example of RAW LCD display using PCF85176
+ */
+
+#include "SegLCD_PCF8576_4Seg6SegMaintSegBatUnits.h"
+
+#include <Wire.h>
+
+SegLCD_PCF8576_4Seg6SegMaintSegBatUnits lcd(Wire);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Initilalize I2C...");
+  Wire.begin(1, 2);
+  pinMode(10, OUTPUT);
+  digitalWrite(10, HIGH);
+
+  Serial.println("Initialize LCD...");
+
+  // Demo for static one segment LCD display
+  lcd.init();
+
+  // Clear LCD
+  lcd.clear();
+
+  lcd.setSignalLevel(1);
+  delay(1000);
+  lcd.setSignalLevel(2);
+  delay(1000);
+  lcd.setSignalLevel(3);
+  delay(1000);
+  lcd.setSignalLevel(4);
+  delay(1000);
+  lcd.setSignalLevel(0);
+
+  delay(1000);
+  lcd.print("5");
+  lcd.print("6");
+  lcd.print("7");
+  lcd.print("8");
+
+  lcd.setCursor(1, 0);
+  lcd.print("1");
+  lcd.print("2");
+  lcd.print("3");
+  lcd.print("4");
+  lcd.print("5");
+  lcd.print("6");
+
+//  lcd.writeRam((uint8_t)0xFF, 13);
+  return;
+  delay(1000);
+
+  // Write at address 0 one byte
+  lcd.writeRam((uint8_t)0xF0, 0);
+
+  delay(1000);
+
+  // Write at address 8 one byte
+  lcd.writeRam((uint8_t)0xF0, 8);
+}
+
+
+void loop() {
+
+}
