6d44d98d6665acb52dfb3e2057893f73b82b00fa
Petr Kracik <petrkr@petrkr.net>
2026-01-18 19:12:15 +0100

    RawLCD use more generic PCx85 class

---
+++ src/SegLCD_PCx85_Raw.cpp
@@ -0,0 +1,19 @@
+#include <Wire.h>
+#include <SegLCD_PCx85_Raw.h>
+
+
+SegLCD_PCx85_Raw::SegLCD_PCx85_Raw(TwoWire& i2c, uint8_t address, uint8_t subaddress) : SegDriver_PCx85(i2c, address, subaddress) {}
+
+void SegLCD_PCx85_Raw::init(ModeDrive drive, ModeBias bias) {
+    SegDriver_PCx85::init();
+    _setMode(MODE_STATUS_ENABLED, drive, bias);
+}
+
+// For generic class allow access to low level functions
+void SegLCD_PCx85_Raw::writeRam(uint8_t data, uint8_t address) {
+    _writeRam(data, address);
+}
+
+void SegLCD_PCx85_Raw::writeRam(uint8_t *data, size_t length, uint8_t address) {
+    _writeRam(data, length, address);
+}
--- src/SegLCD_PCx85_Raw.h
+++ src/SegLCD_PCx85_Raw.h
@@ -5,9 +5,9 @@
 #include <SegDriver_PCx85.h>
 
 
-class SegLCD_PCF85176_Raw : public SegDriver_PCF85176 {
+class SegLCD_PCx85_Raw : public SegDriver_PCx85 {
     public:
-        SegLCD_PCF85176_Raw(TwoWire& i2c, uint8_t address = DEFAULT_PCF85176_I2C_ADDRESS, uint8_t subaddress = DEFAULT_SUBADDRESS);
+        SegLCD_PCx85_Raw(TwoWire& i2c, uint8_t address = DEFAULT_PCF85176_I2C_ADDRESS, uint8_t subaddress = DEFAULT_SUBADDRESS);
         void init(ModeDrive drive, ModeBias bias);
         void writeRam(uint8_t data, uint8_t address = 0);
         void writeRam(uint8_t *data, size_t length, uint8_t address = 0);
