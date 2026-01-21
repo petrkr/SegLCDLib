36628e267e5eac116fc2afca8e5e8be2af3e4841
Petr Kracik <petrkr@petrkr.net>
2026-01-21 19:57:47 +0100

    VK0192: Example decimals

--- examples/VK0192/5DigSigBattProgress/5DigSigBattProgress.ino
+++ examples/VK0192/5DigSigBattProgress/5DigSigBattProgress.ino
@@ -27,29 +27,67 @@ void setup() {
 
   // Clear LCD
   lcd.clear();
-
-  lcd.setCursor(0,0);
-  lcd.print("1.2.3");
+  lcd.setSignalLevel(4);
   delay(1000);
-  lcd.setCursor(0,1);
-  lcd.print("5");
+  lcd.setCursor(2, 0);
+  lcd.print(" . . . .");
+  delay(1000);
+  lcd.setCursor(0, 3);
+  lcd.print("12");
+  delay(1000);
+
+  lcd.setCursor(2, 0);
+  lcd.print("12.34.5");
+  delay(2000);
+  lcd.setCursor(2, 2);
+  lcd.print("5"); // Should persist dot at digit 2
+
+  delay(2000);
+  lcd.setCursor(2, 2);
+  lcd.print("88"); // Should persist dot at digit 2 and remove dot at digit 3 and 4
+
+  delay(2000);
+  lcd.setCursor(2, 0);
+  lcd.print("34"); // Should remove dot at digit 1
+
+  delay(2000);
+  lcd.clear();
+  lcd.setCursor(2, 0);
+  lcd.print(" . . . .");
+
+  delay(2000);
+
+  lcd.setCursor(0, 0);
+  lcd.print("12.356");
 
   lcd.setCursor(1, 0);
   lcd.print("12.3.4.5");
-  delay(1000);
-  lcd.setCursor(1, 2);
-  lcd.print("67");
 
+  delay(2000);
+  lcd.setCursor(0, 2);
+  lcd.print("5"); // Should persist dot at digit 2
+
+  delay(2000);
+  lcd.setCursor(0, 1);
+  lcd.print("3"); // Should remove dot at digit 2
+
+  delay(2000);
+  lcd.setCursor(1, 2);
+  lcd.print("88"); // Should persist dot at digit 2 and remove dot at digit 3 and 4
+
+  delay(2000);
+  lcd.setCursor(1, 0);
+  lcd.print("34"); // Should remove dot at digit 1
+
+  delay(2000);
   lcd.setLabels(SegLCD_VK0192_5DigSigBattProgress::LABEL_C);
 
   lcd.setCursor(2, 0);
-  lcd.print("Hello");
-
+  lcd.print("H.e.l.l.o");
 
   delay(1000);
-  //lcd.clearLabels(SegLCD_VK0192_5DigSigBattProgress::LABEL_M);
 
-  for (int i=0; i <= 100; i+=10) {
+  for (int i=0; i <= 150; i+=10) {
     lcd.setProgress(i);
     delay(500);
   }
