0c798905fb3480c13f51f4395c3b9df0b1641167
Petr Kracik <petrkr@petrkr.net>
2026-01-21 19:56:49 +0100

    VK0192: Raw LCD

---
+++ examples/VK0192/Raw/Raw.ino
@@ -0,0 +1,29 @@
+
+#include "SegLCD_VK0192_Raw.h"
+
+#define PIN_CS 9
+#define PIN_WR 47
+#define PIN_DATA 21
+#define PIN_PWR 15 // If you have connected Vcc on GPIO instead power line else -1
+
+SegLCD_VK0192_Raw raw(PIN_CS, PIN_DATA, PIN_WR);
+
+void setup() {
+  if (PIN_PWR > -1) {
+    raw.initBacklight(PIN_PWR);
+    raw.setBacklight(true);
+  }
+
+  raw.init();
+  raw.clear();
+
+  raw.writeRam(0x80, 0x24); // Digit 1 (col 0)
+  raw.writeRam(0x80, 0x10); // Digit 2 (col 1)
+  raw.writeRam(0x80, 0x1E); // Digit 3 (col 2)
+  raw.writeRam(0x80, 0x28); // Digit 4 (col 3)
+}
+
+
+void loop() {
+  
+}
