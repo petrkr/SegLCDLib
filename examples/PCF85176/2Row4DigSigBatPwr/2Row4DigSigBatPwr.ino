5b75a93836dd376e7bbcb64be1236fd33781aff7
Petr Kracik <petrkr@petrkr.net>
2026-02-17 15:28:19 +0100

    2Row4DigSigBatPwr: example

---
+++ examples/PCF85176/2Row4DigSigBatPwr/2Row4DigSigBatPwr.ino
@@ -0,0 +1,40 @@
+/**
+ * @example PCF85176/RawLCD/RawLCD.ino
+ * @brief Example of RAW LCD display using PCx85
+ */
+
+#include "SegLCD_PCF85176_2Row4DigSigBatPwr.h"
+
+#include <Wire.h>
+
+SegLCD_PCF85176_2Row4DigSigBatPwr lcd(Wire);
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Initilalize I2C...");
+  Wire.begin(1, 2);
+
+  Serial.println("Initialize LCD...");
+
+  // Demo for static one segment LCD display
+  lcd.init();
+
+  // Clear LCD
+  lcd.clear();
+
+  delay(1000);
+
+  // Write at address 0 one byte
+  lcd.print("1.2.34");
+  lcd.print("123.4");
+
+  lcd.setSignalLevel(1);
+  lcd.setBatteryLevel(2);
+  lcd.setPowerSymbol(true);
+}
+
+
+void loop() {
+
+}
